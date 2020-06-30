8bcf5b4a1e2a70a851087e765d2c11c3e466a46f
w00dbury <w00dbury@users.noreply.github.com>
2020-06-30 09:39:41 -0700

    function to send payload to new hooks

--- class-b2c-token-checker.php
+++ class-b2c-token-checker.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 use \Firebase\JWT\JWT;
 use \phpseclib\Crypt\RSA;
@@ -7,65 +7,70 @@ use \phpseclib\Crypt\RSA;
  * A class to verify an id_token, following the implicit flow
  * defined by the OpenID Connect standard.
  */
-class B2C_Token_Checker {
-	
+class B2C_Token_Checker
+{
+
 	private $id_token_array = array(); // still encoded
 	private $head = array(); // decoded
 	private $payload = array(); // decoded
 	private $clientID = '';
-	private $endpoint_handler; 
-	
-	function __construct($id_token, $clientID, $policy_name) {
-		
+	private $endpoint_handler;
+
+	function __construct($id_token, $clientID, $policy_name)
+	{
+
 		$this->clientID = $clientID;
 		$this->endpoint_handler = new B2C_Endpoint_Handler($policy_name);
 		$this->split_id_token($id_token);
 	}
-	
+
 	/** 
 	 * Converts base64url encoded string into base64 encoded string.
 	 * Also adds the necessary padding to the base64 encoded string.
 	 */
-	private function convert_base64url_to_base64($data) {
+	private function convert_base64url_to_base64($data)
+	{
 		return str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT);
 	}
-	
+
 	/**
 	 * Splits the id token into an array of header, payload, and signature.
 	 */
-	private function split_id_token($id_token) {
+	private function split_id_token($id_token)
+	{
 
 		// Split the token into Header, Payload, and Signature, and decode
 		$this->id_token_array = explode('.', $id_token, 3);
 		$this->head = json_decode(base64_decode($this->id_token_array[0]), true);
 		$this->payload = json_decode(base64_decode($this->id_token_array[1]), true);
 	}
-	
+
 	/** 
 	 * Validates the RSA signature on the token.
 	 */
-	private function validate_signature() {
-		
+	private function validate_signature()
+	{
+
 		//require_once "phpseclib/Crypt/RSA.php";
-		
+
 		// Get kid from header
 		$kid = $this->head['kid'];
-		
+
 		// For each JwksURI, get the public key and verify the signature
 		$key_datas = $this->endpoint_handler->get_jwks_uri_data();
 		foreach ($key_datas as $key_data) {
-			
+
 			// Iterate through each key type until the one that matches the "kid" is found
 			$keys = json_decode($key_data, true)['keys'];
 			foreach ($keys as $key) {
-				
+
 				if ($key['kid'] == $kid) {
 					$e = $key['e'];
 					$n = $key['n'];
-					break; 
+					break;
 				}
 			}
-			
+
 			// 'e' and 'n' are base64 URL encoded, change to just base64 encoding
 			$e = $this->convert_base64url_to_base64($e);
 			$n = $this->convert_base64url_to_base64($n);
@@ -77,36 +82,35 @@ class B2C_Token_Checker {
 				<Exponent>' . $e . '</Exponent>
 				</RSAKeyValue>');
 			$public_key = $rsa->getPublicKey();
-			
+
 			// Construct data and signature for verification
 			$to_verify_data = $this->id_token_array[0] . "." . $this->id_token_array[1];
 			$to_verify_sig = base64_decode($this->convert_base64url_to_base64(($this->id_token_array[2])));
-			
+
 			try {
-				$jwt = $this->id_token_array[0] . "." . $this->id_token_array[1] . "." .$this->id_token_array[2];
+				$jwt = $this->id_token_array[0] . "." . $this->id_token_array[1] . "." . $this->id_token_array[2];
 				$decoded = JWT::decode($jwt, $public_key, array('HS256', 'HS384', 'HS512', 'RS256'));
-			}
-			catch (Exception $e) {
+			} catch (Exception $e) {
 				echo 'error: ' . $e->getMessage();
 				return false;
 			}
-			
 		}
-		
+
 		// Returns true when verified successfully
-		return true; 
+		return true;
 	}
-	
+
 	/**
 	 * Validates audience, not_before, expiration_time, and issuer claims.
 	 */
-	private function validate_claims() {
-		
+	private function validate_claims()
+	{
+
 		$audience = $this->payload['aud']; // Should be app's clientID
 		if ($audience != $this->clientID) {
 			return false;
 		}
-		
+
 		$cur_time = time();
 		$not_before = $this->payload['nbf']; // epoch time, time after which token is valid (so basically nbf < cur time < exp)
 		$expiration = $this->payload['exp']; // epoch time, check that the token is still valid
@@ -116,22 +120,23 @@ class B2C_Token_Checker {
 		if ($cur_time > $expiration) {
 			return false;
 		}
-		
+
 		// The Issuer Identifier for the OpenID Provider MUST exactly match the value of the iss (issuer) Claim.
-		$iss_token = $this->payload['iss']; 
+		$iss_token = $this->payload['iss'];
 		$iss_metadata = $this->endpoint_handler->get_issuer();
 		if ($iss_token != $iss_metadata) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	/** 
 	 * Verifies both the signature and claims of the ID token.
 	 */
-	public function authenticate() {
-		
+	public function authenticate()
+	{
+
 		if ($this->validate_signature() == false) {
 			return false;
 		}
@@ -140,13 +145,20 @@ class B2C_Token_Checker {
 		}
 		return true;
 	}
-	
+
 	/** 
 	 * Extracts a claim from the ID token.
 	 */
-	public function get_claim($name) {
+	public function get_claim($name)
+	{
 		return $this->payload[$name];
 	}
+
+	/**
+	 * Returns the payload.
+	 */
+	public function get_payload()
+	{
+		return $this->payload;
+	}
 }
-
-
